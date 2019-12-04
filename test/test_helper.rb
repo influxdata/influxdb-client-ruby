a4cd75b9f939b392131706be98a96a1ce81a9476
Jakub Bednář <jakub.bednar@gmail.com>
2019-12-04 14:10:38 +0100

    feat: add CodeCov integration (#2)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,11 @@
+require 'simplecov'
+SimpleCov.start
+
+if ENV['CI'] == 'true'
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
+
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'influxdb_client'
 
